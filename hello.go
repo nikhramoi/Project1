9a98751d5816b19f968617e337072dcfd645dadf
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-09-22 10:44:32 +0700

    Функции и параметры

--- hello.go
+++ hello.go
@@ -3,34 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	for i := 1; i < 10; i++ {
-		fmt.Println(i * i)
-	}
+	hello()
+	add(1, 2, 3.4, 5.6, 1.2)
+	var a = 8
+	fmt.Println("a before: ", a)
+	increment(a)
+	fmt.Println("a after: ", a)
+	add2(5, 6, 7, 2, 3) // sum = 23
+	add2([]int{1, 2, 3, 4}...)
+}
 
-	var i = 1
-	for i < 10 {
-		fmt.Println(i * i)
-		i++
-	}
+func hello() {
+	fmt.Println("Hello MIR")
+}
 
-	for i := 1; i < 10; i++ {
-		for j := 1; j < 10; j++ {
-			fmt.Print(i*j, "\t")
-		}
-		fmt.Println()
-	}
+func add(x, y int, a, b, c float32) {
+	var z = x + y
+	var d = a + b + c
+	fmt.Println("x + y = ", z)
+	fmt.Println("a + b + c = ", d)
+}
 
-	string1 := "Hello"
-	for index, value := range string1 {
-		fmt.Printf("Indexes: %d, Values: %c\n", index, value)
-	}
+func increment(x int) {
 
-	str := "Hello"
-	for _, value := range str {
-		fmt.Printf("%c ", value)
-	}
-	fmt.Printf("%c ", 10)
+	fmt.Println("x before: ", x)
+	x = x + 20
+	fmt.Println("x after: ", x)
+}
 
-	// Операторы break и continue работают так же, как и в других языках программирования
-	//break OuterLoop - выход из внешнего цикла
+func add2(numbers ...int) {
+	var sum = 0
+	for _, number := range numbers {
+		sum += number
+	}
+	fmt.Println("sum = ", sum)
 }
