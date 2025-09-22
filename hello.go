b07dec1993b557904d70e4e1f0bece805b1351e9
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-09-22 12:57:40 +0700

    Примеры функция как параметр

--- hello.go
+++ hello.go
@@ -3,48 +3,51 @@ package main
 import "fmt"
 
 func main() {
-	var f func(int, int) int = add //Присваиваем f функцию add()
-	fmt.Println(f(3, 4))
-	var x = f(4, 5) // 9
-	fmt.Println(x)
+	slice := []int{-2, 4, 3, -1, 7, -4, 23}
 
-	f2 := add2            //или так var f func(int, int) int = add
-	fmt.Println(f2(3, 4)) // 7
+	sumOfEvens := sum(slice, isEven) // сумма четных чисел
+	fmt.Println(sumOfEvens)          // -2
 
-	f = multiply         // теперь переменная f указывает на функцию multiply
+	sumOfPositives := sum(slice, isPositive) // сумма положительных чисел
+	fmt.Println(sumOfPositives)              // 37
+	//Пример 2
+	f := selectFn(1)
+	fmt.Println(f(3, 4)) // 7
+
+	f = selectFn(3)
 	fmt.Println(f(3, 4)) // 12
 
-	// f = display      // ошибка, так как функция display имеет тип func(string)
-
-	var f1 func(string) = display // норм
-	f1("hello")
-
-	action(10, 25, add3)    // 35
-	action(5, 6, multiply3) // 30
 }
 
-func add(x int, y int) int {
-	return x + y
+func isEven(n int) bool {
+	return n%2 == 0
+}
+func isPositive(n int) bool {
+	return n > 0
 }
 
-func add2(x int, y int) int     { return x + y }
+func sum(numbers []int, criteria func(int) bool) int {
+
+	result := 0
+	for _, val := range numbers {
+		if criteria(val) {
+			result += val
+		}
+	}
+	return result
+}
+
+// Пример возврата функции как результата выполнения другой функции
+func add(x int, y int) int      { return x + y }
+func subtract(x int, y int) int { return x - y }
 func multiply(x int, y int) int { return x * y }
 
-func display(message string) {
-	fmt.Println(message)
-}
-
-//Функции как параметры других функций
-func add3(x int, y int) int {
-
-	return x + y
-}
-func multiply3(x int, y int) int {
-
-	return x * y
-}
-func action(n1 int, n2 int, operation func(int, int) int) {
-
-	result := operation(n1, n2)
-	fmt.Println(result)
+func selectFn(n int) func(int, int) int {
+	if n == 1 {
+		return add
+	} else if n == 2 {
+		return subtract
+	} else {
+		return multiply
+	}
 }
