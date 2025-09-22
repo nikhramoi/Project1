46aed43c306edb30263ee84d8f92cf2f53043566
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-09-22 11:29:13 +0700

    Возвращение результата из функции

--- hello.go
+++ hello.go
@@ -3,38 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	hello()
-	add(1, 2, 3.4, 5.6, 1.2)
-	var a = 8
-	fmt.Println("a before: ", a)
-	increment(a)
-	fmt.Println("a after: ", a)
-	add2(5, 6, 7, 2, 3) // sum = 23
-	add2([]int{1, 2, 3, 4}...)
+	/* func имя_функции (список_параметров) тип_возвращаемого_значения {
+	    выполняемые_операторы
+	    return возвращаемое_значение
+		} */
+	var a = add(4, 5)  // 9
+	var b = add(20, 6) // 26
+	fmt.Println(a)
+	fmt.Println(b)
+	var age, name = add3(4, 5, "Tom", "Simpson")
+	fmt.Println(age)  // 9
+	fmt.Println(name) // Tom Simpson
 }
 
-func hello() {
-	fmt.Println("Hello MIR")
+func add(x, y int) int {
+	return x + y
 }
 
-func add(x, y int, a, b, c float32) {
-	var z = x + y
-	var d = a + b + c
-	fmt.Println("x + y = ", z)
-	fmt.Println("a + b + c = ", d)
+// Именованные возвращаемые результаты
+func add2(x, y int) (z int) {
+	z = x + y
+	return
 }
 
-func increment(x int) {
-
-	fmt.Println("x before: ", x)
-	x = x + 20
-	fmt.Println("x after: ", x)
+func add3(x, y int, firstName, lastName string) (int, string) {
+	var z int = x + y
+	var fullName = firstName + " " + lastName
+	return z, fullName
 }
 
-func add2(numbers ...int) {
-	var sum = 0
-	for _, number := range numbers {
-		sum += number
-	}
-	fmt.Println("sum = ", sum)
-}
+/* func add4(x, y int, firstName, lastName string) (z int, fullName string) {
+    z = x + y
+    fullName = firstName + " " + lastName
+    return
+} */
