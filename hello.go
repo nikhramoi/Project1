6a4278733767397065bb07b8398a3a4dd80ae69a
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-10-08 15:34:57 +0700

    Ошибки

--- hello.go
+++ hello.go
@@ -1,45 +1,25 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	_ "math"
-	"reflect"
 )
 
-type Secret struct {
-	Username string
-	Password string
-}
-type Record struct {
-	Field1 string
-	Field2 float64
-	Field3 Secret
+func main() {
+	result, err := devide(10, 0)
+	if err != nil {
+		log.Fatalf("Devide error %s", err)
+	}
+	fmt.Printf("Результат деления: %s .", result)
 }
 
-func main() {
-	A := Record{"String value", -12.123, Secret{"Mihalis", "Tsoukalos"}}
-	r := reflect.ValueOf(A)
-	fmt.Println("String value:", r.String())
-	iType := r.Type()
-	fmt.Printf("i Type: %s\n", iType)
-	fmt.Printf("The %d fields of %s are\n", r.NumField(), iType)
-	for i := 0; i < r.NumField(); i++ {
-		fmt.Printf("\t%s ", iType.Field(i).Name)
-		fmt.Printf("\twith type: %s ", r.Field(i).Type())
-		fmt.Printf("\tand value _%v_\n", r.Field(i).Interface())
-		// проверьте, есть ли другие структуры, встроенные в запись
-		k := reflect.TypeOf(r.Field(i).Interface()).Kind()
-		// нужно преобразование в строку, чтобы можно было сравнить
-		if k.String() == "struct" {
-			//Чтобы проверить тип данных переменной с помощью строки, нам нужно сначала преобразовать тип данных в string.
-			fmt.Println(r.Field(i).Type())
-		}
-		// то же, что и раньше, но с использованием внутреннего значения
-		if k == reflect.Struct {
-			//Во время проверки вы также можете использовать внутреннее представление типа данных. Однако смысла в этом меньше, чем в использовании значения string.
-			fmt.Println(r.Field(i).Type())
-		}
+func devide(n1, n2 int) (int, error) {
+	if n2 == 0 {
+		return 0, errors.New("devide by 0")
 	}
+	return n1 / n2, nil
 }
 
 // Golang для профи с 39
