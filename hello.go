2bee5d4e56f19a6634adf6ff8ca6cb117b637cf6
nikita hramchenko <nikita.hramchenko@mail.ru>
2025-10-09 10:25:57 +0700

    Замыкания

--- hello.go
+++ hello.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"log"
+	_ "log"
 	_ "math"
-	"strings"
+	_ "strings"
 )
 
 type Person struct {
@@ -14,48 +13,16 @@ type Person struct {
 }
 
 func main() {
-	message, err := UserProfileToString("Никита", -5)
-	if err != nil {
-		log.Fatalf("Devide error %s", err)
-		//fmt.Errorf("Devide error %s", err)
-	}
-	fmt.Println(message)
+	add := adder(10)
+	add(5)
+	add(15)
 }
 
-func UserProfileToString(name string, age int) (string, error) {
-
-	if name == "" {
-		return "", errors.New("empty name")
+func adder(n int) func(int) int {
+	sum := n
+	return func(x int) int {
+		sum += x
+		fmt.Println(sum)
+		return sum
 	}
-	if age < 0 {
-		return "", errors.New("negative age")
-	}
-	if strings.TrimSpace(name) == "" {
-		return "", errors.New("name cannot contain only spaces")
-	}
-	return fmt.Sprintf("Имя человека: %s, возраст: %d.", name, age), nil
 }
-
-func calculate(var1, var2 float64, operation string) (float64, error) {
-	if operation == "add" {
-		return var1 + var2, nil
-	}
-	if operation == "subtract" {
-		return var1 - var2, nil
-	}
-
-	if operation == "multiply" {
-		return var1 * var2, nil
-	}
-
-	if operation == "divide" {
-		if var2 == 0 {
-			return 0, errors.New("division by zero")
-		}
-		return var1 / var2, nil
-	}
-
-	return 0, errors.New("unknown operation")
-}
-
-// Golang для профи с 39
